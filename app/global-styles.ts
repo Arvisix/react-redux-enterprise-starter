a035372fe68133cf678f99460d8ec95709782570
Maxim Moroz <maxm.morz@gmail.com>
2019-12-11 19:35:28 -0600

    Moved images folder

--- app/global-styles.ts
+++ app/global-styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styles/styled-components';
+import { createGlobalStyle } from 'modules/App/styles/styled-components';
 
 const GlobalStyle = createGlobalStyle`
   html,
